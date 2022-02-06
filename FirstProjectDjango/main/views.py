70fdb3c49215d779b11ba93e16edad6e5ab30dda
pavelrudenia <pavelrudeniaaa@gmail.com>
2022-02-06 13:28:36 +0300

    Initial commit

---
+++ FirstProjectDjango/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    data={
+        'title': 'Главная страница',
+        'values':['Some','Hello','123']
+    }
+    return render(request, 'main/index.html',data)
+
+
+def about(request):
+    return render(request,'main/about.html')
